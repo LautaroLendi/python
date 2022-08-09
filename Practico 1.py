eb327e3b436ff6cca85a3ab272333462818b431a
LautaroLendi <110908132+LautaroLendi@users.noreply.github.com>
2022-08-09 11:15:04 -0300

    UP

---
+++ Practico 1.py
@@ -0,0 +1,9 @@
+print("-------------------------------------------------------")
+print("Ejercicio1: CALCULAR DISTANCIA.")
+print("-------------------------------------------------------")
+
+print("Ingrese la velocidad y el tiempo de la unidad móvil")
+
+V = float( input("Velocidad: ") )
+T = int( input("Tiempo: ") ) 
+D = V*T 
